4d2a17737b03266ce5038de2ee9abd9444d7e8e1
hchann <yhc8089@naver.com>
2022-04-18 16:01:41 +0900

    7주차 수업내용

--- Assets/p01.cs
+++ Assets/p01.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movecube : MonoBehaviour
+public class p01 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+           
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-       
+        
     }
-}    
+}
--- Assets/p02.cs
+++ Assets/p02.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movecube : MonoBehaviour
+public class p02 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        Vector2 playerPos = new Vector2(3.0f, 4.0f);
+        playerPos.x += 8.0f;
+        playerPos.y += 5.0f;
+        Debug.Log(playerPos);
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-       
+        
     }
-}    
+}
